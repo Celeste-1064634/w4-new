8dd45b7380c64e8d90f1d517f92c2cee8241bcc1
Marinda-1058790 <113016517+Marinda-1058790@users.noreply.github.com>
2023-05-22 11:14:35 +0200

    UPDATE: Added functionality to button

--- frontend/src/pages/NewSurveyMaker.js
+++ frontend/src/pages/NewSurveyMaker.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
-import styles from './NewSurveyMaker.module.css'
-import { Button } from 'react-bootstrap'
+import React, { useState } from "react";
+import styles from "./NewSurveyMaker.module.css";
+import { Button } from "react-bootstrap";
+import NewQuestionItem from "./NewQuestionItem";
 
 function NewSurveyMaker() {
   const [addQuestion, setAddQuestion] = useState(false);
@@ -8,18 +9,23 @@ function NewSurveyMaker() {
   function addNewQuestion() {
     setAddQuestion((addQuestion) => !addQuestion);
   }
-  
-  console.log(addQuestion)
-    return (
+
+  console.log(addQuestion);
+  return (
     <>
-    <div className={styles.divPageTitle}>
-    <h1 >Nieuwe vragenlijst</h1>
-    </div>
-    <div className={styles.divAddNewQuestion}>
-        <Button onClick={addNewQuestion} className={styles.btnAddNewQuestion}>+</Button>
-    </div>
+      <div className={styles.divPageTitle}>
+        <h1>Nieuwe vragenlijst</h1>
+      </div>
+      <div className={styles.divAddNewQuestion}>
+        <Button onClick={addNewQuestion} className={styles.btnAddNewQuestion}>
+          +
+        </Button>
+        {addQuestion
+          ? <NewQuestionItem/>
+          : null}
+      </div>
     </>
-  )
+  );
 }
 
-export default NewSurveyMaker
+export default NewSurveyMaker;
